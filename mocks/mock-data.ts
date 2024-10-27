bfd3ef361ce44f5ca7c22eb1d112ea4ed2ef7991
an.s.belousova <an.s.belousova@tinkoff.ru>
2024-10-27 17:56:29 +0500

    add mock

---
+++ mocks/mock-data.ts
@@ -0,0 +1,2 @@
+Красный дракон	Проживание в новом отеле в китайском стиле	2024.04.24	Amsterdam	dragon_logo.jpg	room1.jpg/room2.jpg/room3.jpg	false	true	4.8	apartment	3	4	200	Breakfast/Laptop friendly workspace	Кот	5	5.0000/2.0000
+Эйфель	Апартаменты в стиле франции	2024.10.01	Paris	paris.jpg	ap1.jpg/ap2.jpg/ap3.jpg/ap4.jpg	true	false	3.1	house	2	2	20000	Fridge	Эйфель	3	50.000/40.0000
