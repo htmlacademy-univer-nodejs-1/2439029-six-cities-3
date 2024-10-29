da751ede2061775058768a57efe9d5dd182667cb
an.s.belousova <an.s.belousova@tinkoff.ru>
2024-10-30 01:30:40 +0500

    refactor generate

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -11,8 +11,9 @@ export class HelpCommand implements Command {
 Пример: cli.js --<command> [--arguments]
 По факту:  npm run ts ./src/main.cli.ts -- <command> [--arguments]\n`,
         chalk.yellow('Команды:\n'),
-        chalk.green('\t--help:                      '), chalk.inverse('# выводит информации о версии приложения\n'),
-        chalk.green('\t--version:                   '), chalk.inverse('# выводит информацию о списке поддерживаемых команд\n'),
-        chalk.green('\t--import <path>:             '), chalk.inverse('# импортирует данные из *.tsv-файла\n'));
+        chalk.green('\t--help:                         '), chalk.inverse('# выводит информации о версии приложения\n'),
+        chalk.green('\t--version:                      '), chalk.inverse('# выводит информацию о списке поддерживаемых команд\n'),
+        chalk.green('\t--import <path>:                '), chalk.inverse('# импортирует данные из *.tsv-файла\n'),
+        chalk.green('\t--generate <count> <path> <url>:'), chalk.inverse('# генерирует произвольное количество тестовых наборов (предложений по аренде)\n'));
   }
 }
