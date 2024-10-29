29f1f82d17450430189b1e9b331a1c34fab7218a
an.s.belousova <an.s.belousova@tinkoff.ru>
2024-10-29 13:19:16 +0500

    add MockServerData

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -5,3 +5,4 @@ export * from './housing.enum.js';
 export * from './city.enum.js';
 export * from './user.type.js';
 export * from './offer.type.js';
+export * from './mock-server-data.type.js'
---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,11 @@
+export type MockServerData = {
+  name: string[];
+  description: string[];
+  previewImg: string[];
+  images: string[];
+  price: string[];
+  facilities: string[];
+  countComments: string[];
+  author: string[];
+  coordinates: string[];
+};
