73e6d9f909016cefd002ca2c7f27c6528b1ff90c
an.s.belousova <an.s.belousova@tinkoff.ru>
2024-11-11 18:54:22 +0500

    add createdAt, updatedAt

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,7 +1,7 @@
 import { User } from '../../types/index.js';
-import {getModelForClass, prop} from '@typegoose/typegoose';
+import {defaultClasses, getModelForClass, prop} from '@typegoose/typegoose';
 
-export class UserEntity implements User {
+export class UserEntity extends defaultClasses.TimeStamps implements User {
   @prop({ required: false, default: "avatar.jpg", match: [/\.(jpg|png)$/, 'Avatar image format jpg or png'],})
   avatar?: string;
 
