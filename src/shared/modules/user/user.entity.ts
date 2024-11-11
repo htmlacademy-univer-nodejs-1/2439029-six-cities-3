1b5ba596aecdcf7fef565904ba12ffcc3e4087c7
an.s.belousova <an.s.belousova@tinkoff.ru>
2024-11-11 18:50:51 +0500

    add user by typegoose

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
 import { User } from '../../types/index.js';
+import {getModelForClass, prop} from '@typegoose/typegoose';
 
 export class UserEntity implements User {
+  @prop({ required: false, default: "avatar.jpg", match: [/\.(jpg|png)$/, 'Avatar image format jpg or png'],})
   avatar?: string;
+
+  @prop({ unique: true, match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'], required: true,})
   email: string;
+
+  @prop({required: true, minlength: [1, 'Min length for name is 1'], maxlength: [15, 'Max length for name is 15'],})
   name: string;
+
+  @prop({required: true, minlength: [6, 'Min length for name is 6'], maxlength: [12, 'Max length for name is 12'],})
   password: string;
+
+  @prop({required: true, enum: ['normal', 'pro'],})
   userType: "normal" | "pro";
 }
+
+export const UserModel = getModelForClass(UserEntity);
